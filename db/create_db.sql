919351cf59fb23fd4dad81d1797f07eb373d8256
Tomas Karella <tomas.karella@gmail.com>
2021-02-26 18:21:41 +0100

    Fixing frontend, adding it to docker compose

---
+++ db/create_db.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE park_db OWNER postgres;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from '@/plugins/vuetify';
 import VueKonva from 'vue-konva'
 import vueFabricWrapper from "vue-fabric-wrapper";
 
